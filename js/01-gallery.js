e9cb3dcee82167b6afdcbc32aa7f2f52a7f5596d
inhasid <sidorchuk.inga@gmail.com>
2023-10-30 09:35:50 +0100

    01-gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,52 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+list.addEventListener("click", handleClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+           />
+        </a>
+      </li>
+  `
+    )
+    .join("");
+}
+
+function handleClick(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+    event.preventDefault();
+    const originalImg = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+  <div class="modal">
+    <img src="${originalImg.original}" alt="${originalImg.description}"/>
+  </div>
+  `);
+  instance.show();
+  document.addEventListener("keydown", onEscKeyPress);
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    document.removeEventListener(
+      "keydown",
+      onEscKeyPress
+    );
+  }
+}
+
+console.log(galleryItems);
